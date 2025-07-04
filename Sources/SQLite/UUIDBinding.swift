0c0edec4784fa2d05f8dbee4802f21f889317054
Alsey Coleman Miller <alseycmiller@gmail.com>
2025-07-04 14:09:00 -0400

    Add `Binding.UUIDFormat`

---
+++ Sources/SQLite/UUIDBinding.swift
@@ -0,0 +1,64 @@
+//
+//  UUIDBinding.swift
+//  SQLite
+//
+//  Created by Alsey Coleman Miller on 7/4/25.
+//
+
+#if canImport(Foundation)
+import Foundation
+
+public extension Binding {
+    
+    enum UUIDFormat: Equatable, Hashable, Sendable, CaseIterable {
+        
+        /// UUID as string
+        case text
+        
+        /// UUID as data blob
+        case blob
+    }
+}
+
+public extension Binding.UUIDFormat {
+    
+    var affinity: TypeAffinity {
+        switch self {
+        case .text:
+            return .text
+        case .blob:
+            return .blob
+        }
+    }
+}
+
+public extension Binding {
+    
+    static func uuid(_ uuid: UUID, type: Binding.UUIDFormat = .blob) -> Binding {
+        type.format(uuid)
+    }
+}
+
+public extension Binding.UUIDFormat {
+    
+    static func format(text uuid: UUID) -> String {
+        uuid.uuidString
+    }
+    
+    static func format(blob uuid: UUID) -> UnsafeRawBufferPointer {
+        withUnsafeBytes(of: uuid.uuid) { buffer in
+            return buffer
+        }
+    }
+    
+    func format(_ uuid: UUID) -> Binding {
+        switch self {
+        case .text:
+            return Self.format(text: uuid).binding
+        case .blob:
+            return Self.format(blob: uuid).binding
+        }
+    }
+}
+
+#endif
