10bc7b4a67abce6878f2fba9ecb8dc365eab39d6
Alsey Coleman Miller <alseycmiller@gmail.com>
2025-07-04 11:34:28 -0400

    Created Swift package

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version: 6.0
+import PackageDescription
+
+let package = Package(
+    name: "SQLite",
+    products: [
+        .library(
+            name: "SQLite",
+            targets: ["SQLite"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "SQLite"
+        ),
+        .testTarget(
+            name: "SQLiteTests",
+            dependencies: ["SQLite"]
+        )
+    ]
+)
