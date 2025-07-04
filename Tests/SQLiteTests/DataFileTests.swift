efa7e3e4efe7463c4164def7751cd1086c5914e7
Alsey Coleman Miller <alseycmiller@gmail.com>
2025-07-04 16:32:48 -0400

    Updated unit tests

--- Tests/SQLiteTests/DataFileTests.swift
+++ Tests/SQLiteTests/DataFileTests.swift
@@ -5,3 +5,24 @@
 //  Created by Alsey Coleman Miller on 7/4/25.
 //
 
+import Foundation
+import Testing
+@testable import SQLite
+
+@Suite struct DataFileTests {
+    
+    @Test func locations() throws {
+        
+    }
+}
+
+private extension DataFileTests {
+    
+    /// Load data file
+    func loadDataFile() throws -> DataFile {
+        guard let path = path(for: "data.sqlite") else {
+            throw CocoaError(.fileNoSuchFile)
+        }
+        return try DataFile(path: path)
+    }
+}
--- Tests/SQLiteTests/SQLiteTests.swift
+++ Tests/SQLiteTests/SQLiteTests.swift
@@ -1,6 +1,31 @@
+import Foundation
 import Testing
 @testable import SQLite
 
-@Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+@Suite struct SQLiteTests {
+    
+    @Test func filename() throws {
+        let fileName = "data.sqlite"
+        guard let path = path(for: fileName) else {
+            throw CocoaError(.fileNoSuchFile)
+        }
+        let connection = try Connection(path: path)
+        #expect(connection.filename == path)
+    }
+}
+
+// MARK: - Supporting Functions
+
+/// Load test file
+func path(for file: String) -> String? {
+    Bundle.module.path(forResource: "TestFiles/" + file, ofType: nil)
+}
+
+/// Read the specified test file and load data.
+func data(for file: String) throws -> Data {
+    guard let path = path(for: file) else {
+        throw CocoaError(.fileNoSuchFile)
+    }
+    let url = URL(fileURLWithPath: path)
+    return try Data(contentsOf: url, options: [.mappedIfSafe])
 }
