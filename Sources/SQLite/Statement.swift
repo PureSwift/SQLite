5f5df249540411219fec89719c3124617360e8b6
Alsey Coleman Miller <alseycmiller@gmail.com>
2025-07-04 16:19:27 -0400

    Fixed `Statement` error handling

--- Sources/SQLite/Statement.swift
+++ Sources/SQLite/Statement.swift
@@ -20,10 +20,6 @@ public struct Statement: ~Copyable {
     
     let handle: Handle
     
-    init(handle: Handle) {
-        self.handle = handle
-    }
-    
     deinit {
         handle.finalize()
     }
@@ -45,9 +41,10 @@ public extension Statement {
         handle.columnName(at: Int32(index))
     }
     
-    func bind(_ binding: Binding, at index: Int) throws(SQLiteError) {
+    /// Bind data at the specified index.
+    func bind(_ binding: Binding, at index: Int, connection: borrowing Connection) throws(SQLiteError) {
         let index = Int32(index)
-        return try handle.bind(binding, at: index).get()
+        return try handle.bind(binding, at: index, connection: connection.handle).get()
     }
 }
 
@@ -74,66 +71,54 @@ internal extension Statement.Handle {
         String(cString: sqlite3_column_name(pointer, index))
     }
     
-    func bindNull(at index: Int32) -> Result<Void, SQLiteError> {
-        sqlite3_bind_null(pointer, index).value()
+    func bindNull(at index: Int32, connection: Connection.Handle) -> Result<Void, SQLiteError> {
+        connection.check(sqlite3_bind_null(pointer, index))
     }
     
     /// Binds a BLOB of length N that is filled with zeroes. A zeroblob uses a fixed amount of memory (just an integer to hold its size) while it is being processed.
     /// Zeroblobs are intended to serve as placeholders for BLOBs whose content is later written using incremental BLOB I/O routines.
     ///
     /// A negative value for the zeroblob results in a zero-length BLOB.
-    func bindZeroBlob(at index: Int32, count: Int32) -> Result<Void, SQLiteError> {
-        sqlite3_bind_zeroblob(pointer, index, count).value()
+    func bindZeroBlob(at index: Int32, count: Int32, connection: Connection.Handle) -> Result<Void, SQLiteError> {
+        connection.check(sqlite3_bind_zeroblob(pointer, index, count))
     }
     
-    func bindBlob(_ rawBytes: UnsafeRawPointer, count: Int32, at index: Int32) -> Result<Void, SQLiteError> {
-        sqlite3_bind_blob(pointer, index, rawBytes, count, SQLITE_TRANSIENT).value()
+    func bindBlob(_ rawBytes: UnsafeRawPointer, count: Int32, at index: Int32, connection: Connection.Handle) -> Result<Void, SQLiteError> {
+        connection.check(sqlite3_bind_blob(pointer, index, rawBytes, count, SQLITE_TRANSIENT))
     }
     
-    @available(macOS 10.14.4, iOS 12.2, *)
-    func bindBlob(_ span: RawSpan, at index: Int32) -> Result<Void, SQLiteError> {
-        let count = span.byteCount
-        return span.withUnsafeBytes { buffer in
-            bindBlob(buffer.baseAddress!, count: Int32(count), at: index)
-        }
+    func bindDouble(_ value: Double, at index: Int32, connection: Connection.Handle) -> Result<Void, SQLiteError> {
+        connection.check(sqlite3_bind_double(pointer, index, value))
     }
     
-    func bindDouble(_ value: Double, at index: Int32) -> Result<Void, SQLiteError> {
-        sqlite3_bind_double(pointer, index, value).value()
+    func bindInt(_ value: Int64, at index: Int32, connection: Connection.Handle) -> Result<Void, SQLiteError> {
+        connection.check(sqlite3_bind_int64(pointer, index, value))
     }
     
-    func bindFloat(_ value: Float, at index: Int32) -> Result<Void, SQLiteError> {
-        bindDouble(Double(value), at: index)
+    func bindText(_ value: String, at index: Int32, connection: Connection.Handle) -> Result<Void, SQLiteError> {
+        connection.check(sqlite3_bind_text(pointer, index, value, -1, SQLITE_TRANSIENT))
     }
     
-    func bindInt(_ value: Int64, at index: Int32) -> Result<Void, SQLiteError> {
-        sqlite3_bind_int64(pointer, index, value).value()
-    }
-    
-    func bindInt(_ value: Int, at index: Int32) -> Result<Void, SQLiteError> {
-        bindInt(Int64(value), at: index)
-    }
-    
-    func bindText(_ value: String, at index: Int32) -> Result<Void, SQLiteError> {
-        sqlite3_bind_text(pointer, index, value, -1, SQLITE_TRANSIENT).value()
-    }
-    
-    func bind(_ binding: Binding, at index: Int32) -> Result<Void, SQLiteError> {
+    func bind(_ binding: Binding, at index: Int32, connection: Connection.Handle) -> Result<Void, SQLiteError> {
         switch binding {
         case .null:
-            return bindNull(at: index)
+            return bindNull(at: index, connection: connection)
         case let .integer(value):
-            return bindInt(value, at: index)
+            return bindInt(value, at: index, connection: connection)
         case let .double(value):
-            return bindDouble(value, at: index)
+            return bindDouble(value, at: index, connection: connection)
         case let .text(value):
-            return bindText(value, at: index)
+            return bindText(value, at: index, connection: connection)
         case let .blob(.zero(count)):
-            return bindZeroBlob(at: index, count: count)
+            return bindZeroBlob(at: index, count: count, connection: connection)
         case let .blob(.pointer(block)):
             return block { buffer in
-                bindBlob(buffer.baseAddress!, count: Int32(buffer.count), at: index)
+                bindBlob(buffer.baseAddress!, count: Int32(buffer.count), at: index, connection: connection)
             }
         }
+    }
+    
+    func prepare() {
+        
     }
 }
