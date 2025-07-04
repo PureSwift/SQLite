e1f00bfa8d9ec588c665c1a4e67b55dfe969dbca
Alsey Coleman Miller <alseycmiller@gmail.com>
2025-07-04 11:35:24 -0400

    Add `Statement.ColumnView`

---
+++ Sources/SQLite/StatementColumnView.swift
@@ -0,0 +1,51 @@
+//
+//  StatementColumnView.swift
+//  SQLite
+//
+//  Created by Alsey Coleman Miller on 7/4/25.
+//
+
+public extension Statement {
+    
+    func withColumns<T>(_ block: (borrowing ColumnView) -> (T)) -> T {
+        let view = ColumnView(handle: handle)
+        return block(view)
+    }
+    
+    struct ColumnView {
+        
+        let handle: Handle
+        
+        init(handle: Handle) {
+            self.handle = handle
+        }
+    }
+}
+
+extension Statement.ColumnView: RandomAccessCollection {
+    
+    public typealias Element = String
+    
+    public typealias Index = Int
+
+    public var isEmpty: Bool {
+        count == 0
+    }
+    
+    public var count: Int {
+        handle.columnCount
+    }
+    
+    public var startIndex: Int { 0 }
+    
+    public var endIndex: Int { count }
+    
+    public subscript(position: Int) -> Element {
+        precondition(position >= 0 && position < count, "Index out of bounds")
+        return handle.columnName(at: position)
+    }
+    
+    public func index(after i: Int) -> Int {
+        i + 1
+    }
+}
