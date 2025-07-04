53db33672be51c788a94510ec3c9a38ffa8d8c74
Alsey Coleman Miller <alseycmiller@gmail.com>
2025-07-04 18:38:49 -0400

    Updated unit tests

--- Tests/SQLiteTests/SQLiteTests.swift
+++ Tests/SQLiteTests/SQLiteTests.swift
@@ -39,15 +39,20 @@ import Testing
         #expect(connection.filename == path)
         let sql = "SELECT STATE_NM, STATE_AB FROM state_codes"
         let statement = try Statement(sql, connection: connection)
+        let statementHandle = statement.handle
         // compile statement
-        let results = try statement.withColumns(connection) { (columns: Statement.Column) throws(SQLiteError) -> [String: String] in
+        var results = [[String: String]]()
+        try connection.execute(statement) { (row: consuming Row) throws(SQLiteError) -> () in
+            #expect(results.count == row.index)
             var object = [String: String]()
-            for (index, column) in columns.enumerated() {
-                let text = try statement.handle.readText(at: Int32(index), connection: connection.handle).get()
-                object[column] = text
+            for (columnIndex, column) in row.columns.enumerated() {
+                #expect(columnIndex == column.index)
+                let text = try statementHandle.readText(at: Int32(column.index), connection: connection.handle).get()
+                object[column.name] = text
             }
-            return object
+            results.append(object)
         }
+        
         let expectedValue: [[String: String]] = [["STATE_NM": "ALABAMA", "STATE_AB": "AL"], ["STATE_NM": "ALASKA", "STATE_AB": "AK"], ["STATE_NM": "ARIZONA", "STATE_AB": "AZ"], ["STATE_AB": "AR", "STATE_NM": "ARKANSAS"], ["STATE_AB": "CA", "STATE_NM": "CALIFORNIA"], ["STATE_NM": "COLORADO", "STATE_AB": "CO"], ["STATE_AB": "CT", "STATE_NM": "CONNECTICUT"], ["STATE_AB": "DE", "STATE_NM": "DELAWARE"], ["STATE_NM": "DISTRICT OF COLUMBIA", "STATE_AB": "DC"], ["STATE_AB": "FL", "STATE_NM": "FLORIDA"], ["STATE_NM": "GEORGIA", "STATE_AB": "GA"], ["STATE_NM": "HAWAII", "STATE_AB": "HI"], ["STATE_NM": "IDAHO", "STATE_AB": "ID"], ["STATE_AB": "IL", "STATE_NM": "ILLINOIS"], ["STATE_NM": "INDIANA", "STATE_AB": "IN"], ["STATE_NM": "IOWA", "STATE_AB": "IA"], ["STATE_AB": "KS", "STATE_NM": "KANSAS"], ["STATE_NM": "KENTUCKY", "STATE_AB": "KY"], ["STATE_NM": "LOUISIANA", "STATE_AB": "LA"], ["STATE_AB": "ME", "STATE_NM": "MAINE"], ["STATE_AB": "MD", "STATE_NM": "MARYLAND"], ["STATE_AB": "MA", "STATE_NM": "MASSACHUSETTS"], ["STATE_NM": "MICHIGAN", "STATE_AB": "MI"], ["STATE_AB": "MN", "STATE_NM": "MINNESOTA"], ["STATE_NM": "MISSISSIPPI", "STATE_AB": "MS"], ["STATE_NM": "MISSOURI", "STATE_AB": "MO"], ["STATE_AB": "MT", "STATE_NM": "MONTANA"], ["STATE_NM": "NEBRASKA", "STATE_AB": "NE"], ["STATE_NM": "NEVADA", "STATE_AB": "NV"], ["STATE_AB": "NH", "STATE_NM": "NEW HAMPSHIRE"], ["STATE_AB": "NJ", "STATE_NM": "NEW JERSEY"], ["STATE_AB": "NM", "STATE_NM": "NEW MEXICO"], ["STATE_NM": "NEW YORK", "STATE_AB": "NY"], ["STATE_NM": "NORTH CAROLINA", "STATE_AB": "NC"], ["STATE_AB": "ND", "STATE_NM": "NORTH DAKOTA"], ["STATE_NM": "OHIO", "STATE_AB": "OH"], ["STATE_NM": "OKLAHOMA", "STATE_AB": "OK"], ["STATE_NM": "OREGON", "STATE_AB": "OR"], ["STATE_NM": "PENNSYLVANIA", "STATE_AB": "PA"], ["STATE_NM": "RHODE ISLAND", "STATE_AB": "RI"], ["STATE_NM": "SOUTH CAROLINA", "STATE_AB": "SC"], ["STATE_NM": "SOUTH DAKOTA", "STATE_AB": "SD"], ["STATE_NM": "TENNESSEE", "STATE_AB": "TN"], ["STATE_NM": "TEXAS", "STATE_AB": "TX"], ["STATE_AB": "UT", "STATE_NM": "UTAH"], ["STATE_AB": "VT", "STATE_NM": "VERMONT"], ["STATE_NM": "VIRGINIA", "STATE_AB": "VA"], ["STATE_NM": "WASHINGTON", "STATE_AB": "WA"], ["STATE_NM": "WEST VIRGINIA", "STATE_AB": "WV"], ["STATE_NM": "WISCONSIN", "STATE_AB": "WI"], ["STATE_NM": "WYOMING", "STATE_AB": "WY"], ["STATE_NM": "ONTARIO", "STATE_AB": "ON"]]
         
         #expect(expectedValue == results)
@@ -69,3 +74,4 @@ func data(for file: String) throws -> Data {
     let url = URL(fileURLWithPath: path)
     return try Data(contentsOf: url, options: [.mappedIfSafe])
 }
+
