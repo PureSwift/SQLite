90290e71506f429b83b7a7eb6b5fe0f94a4951e1
Alsey Coleman Miller <alseycmiller@gmail.com>
2025-07-04 16:19:09 -0400

    Add `SQLiteError.ErrorCode`

--- Sources/SQLite/Error.swift
+++ Sources/SQLite/Error.swift
@@ -15,17 +15,64 @@ import SwiftToolchainCSQLite
 import SQLite3
 #endif
 
+/// SQLite Error
 public struct SQLiteError: Error {
     
-    public let errorCode: CInt
+    public let errorCode: ErrorCode
+    
+    public let message: String
+    
+    public let connection: String
+    
+    public let file: StaticString
+    
+    public let function: StaticString
+    
+    internal init(
+        errorCode: ErrorCode,
+        message: String,
+        connection: String,
+        file: StaticString = #file,
+        function: StaticString = #function
+    ) {
+        self.errorCode = errorCode
+        self.message = message
+        self.file = file
+        self.function = function
+        self.connection = connection
+    }
 }
 
-internal extension CInt {
+// MARK: - Supporting Types
+
+public extension SQLiteError {
     
-    func value() -> Result<Void, SQLiteError> {
-        guard self == SQLITE_OK else {
-            return .failure(SQLiteError(errorCode: self))
+    /// Represents a SQLite specific [error code](https://sqlite.org/rescode.html)
+    ///
+    /// - rawValue: SQLite [error code](https://sqlite.org/rescode.html#primary_result_code_list)
+    struct ErrorCode: RawRepresentable, Equatable, Hashable, Sendable {
+        
+        public let rawValue: CInt
+        
+        public init?(rawValue: CInt) {
+            guard Self.isValid(rawValue) else {
+                return nil
+            }
+            self.init(rawValue)
         }
-        return .success(())
+        
+        internal init(_ raw: CInt) {
+            assert(Self.isValid(raw))
+            self.rawValue = raw
+        }
+    }
+}
+
+internal extension SQLiteError.ErrorCode {
+    
+    static let successCodes: Set<SQLiteError.ErrorCode.RawValue> = [SQLITE_OK, SQLITE_ROW, SQLITE_DONE]
+    
+    static func isValid(_ rawValue: RawValue) -> Bool {
+        Self.successCodes.contains(rawValue) == false
     }
 }
